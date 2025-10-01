44c327675d3d321285d7e22912d4ed7fd2d84130
premiumcreativeuser <premiumcreativeuser@proton.me>
2025-10-01 13:06:01 -0700

    Update moveonjoy-epg-grabber.py

--- moveonjoy/moveonjoy-epg-grabber.py
+++ moveonjoy/moveonjoy-epg-grabber.py
@@ -76,7 +76,7 @@ def filter_and_build_epg(urls):
 
 
 urls = [
-    'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_MX1.xml.gz',
